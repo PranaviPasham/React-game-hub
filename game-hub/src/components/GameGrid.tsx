47f7a38d723134916b194a026484d28400e1d488
Pranavi Reddy <pranavireddy@Pranavis-Air.lan>
2024-04-09 23:01:22 -0500

    Filter games by Platform

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -1,11 +1,17 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkelton from "./GameCardSkelton";
 import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+interface Props {
+  selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
+}
+
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
@@ -13,13 +19,13 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3} padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkelton key={skeleton}></GameCardSkelton>
+            <GameCardContainer key={skeleton}>
+              <GameCardSkelton></GameCardSkelton>
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,10 +1,7 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
+import { Platform } from "./usePlatforms";
 
-export interface Platform {
-  id:number;
-  name:string;
-  slug:string;
-}
 
 export interface Game {
     id: number;
@@ -14,6 +11,16 @@ export interface Game {
     metacritic:number;
   }
   
-const useGames = () => useData<Game>('/games')
+const useGames = (selectedGenre : Genre | null, selectedPlatform : Platform | null) => 
+useData<Game>('/games',{
+  params:
+  {
+    genres:selectedGenre?.id, 
+    platforms : selectedPlatform?.id
+  }},
+    [
+      selectedGenre?.id,
+      selectedPlatform?.id
+    ])
 
 export default useGames;
